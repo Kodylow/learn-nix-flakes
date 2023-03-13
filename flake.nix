a82c2869074b758c0c4e0aea4ffed4057cccf0ec
kody.low <kodylow7@gmail.com>
2023-03-13 11:42:22 -0700

    flake works

--- flake.nix
+++ flake.nix
@@ -1,12 +1,11 @@
 {
   description = "A flake for building Hello World";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-20.03;
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }: {
 
     defaultPackage.x86_64-linux =
-      # Notice the reference to nixpkgs here.
       with import nixpkgs { system = "x86_64-linux"; };
       stdenv.mkDerivation {
         name = "hello";
@@ -15,5 +14,61 @@
         installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
       };
 
+    devShell.x86_64-linux = 
+      let pkgs = import nixpkgs { system = "x86_64-linux"; }; in
+      pkgs.mkShell {
+        name = "hello-dev";
+        buildInputs = [ pkgs.gcc ];
+      };
+
+    packages.x86_64-linux =
+      with import nixpkgs { system = "x86_64-linux"; };
+      {
+        hello = defaultPackage.x86_64-linux;
+      };
+
+    defaultPackage.aarch64-linux =
+      with import nixpkgs { system = "aarch64-linux"; };
+      stdenv.mkDerivation {
+        name = "hello";
+        src = self;
+        buildPhase = "gcc -o hello ./hello.c";
+        installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
+      };
+
+    devShell.aarch64-linux = 
+      let pkgs = import nixpkgs { system = "aarch64-linux"; }; in
+      pkgs.mkShell {
+        name = "hello-dev";
+        buildInputs = [ pkgs.gcc ];
+      };
+
+    packages.aarch64-linux =
+      with import nixpkgs { system = "aarch64-linux"; };
+      {
+        hello = defaultPackage.aarch64-linux;
+      };
+
+    defaultPackage.aarch64-darwin =
+      with import nixpkgs { system = "aarch64-darwin"; };
+      stdenv.mkDerivation {
+        name = "hello";
+        src = self;
+        buildPhase = "clang -o hello ./hello.c";
+        installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
+      };
+
+    devShell.aarch64-darwin = 
+      let pkgs = import nixpkgs { system = "aarch64-darwin"; }; in
+      pkgs.mkShell {
+        name = "hello-dev";
+        buildInputs = [ pkgs.clang ];
+      };
+
+    packages.aarch64-darwin =
+      with import nixpkgs { system = "aarch64-darwin"; };
+      {
+        hello = defaultPackage.aarch64-darwin;
+      };
   };
 }
